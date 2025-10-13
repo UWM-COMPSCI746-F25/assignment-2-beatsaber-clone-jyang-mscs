09073b0ce48866cdf4fdbd29f4e55ab6745d58fe
Jefferson Yang <jeffersonyang01@gmail.com>
2025-10-13 17:33:10 -0500

    Update to line reader and button functionality
    Added button functionality to A/X on controllers via node connections. Added a state variable for button pressed of not to trigger when the line reader activates or not.

--- scripts/left_controller.gd
+++ scripts/left_controller.gd
@@ -4,6 +4,8 @@ var velocity = 0
 const RAY_LENGTH = 1
 @onready var lineRender = $"LHMeshInstance3D"
 
+var saberState = false
+
 func _ready():
 	pass
 
@@ -15,21 +17,22 @@ func _process(_delta):
 # for physics simulation
 func _physics_process(_delta):
 	
-	# raycast line
-	var space_state = get_world_3d().direct_space_state
-	var origin = global_position
-	var dir = global_basis.z * -1
-	var end = origin + (dir * RAY_LENGTH)
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-	var result = space_state.intersect_ray(query)
-	# adding the line render to the controller
-	lineRender.points[0] = origin
-	lineRender.points[1] = end
-	
-	if result:
-		lineRender.points[1] = result.position
-		print("Collided with", + result.collider.name)
+	if saberState:
+		# raycast line
+		var space_state = get_world_3d().direct_space_state
+		var origin = global_position
+		var dir = global_basis.z * -1
+		var end = origin + (dir * RAY_LENGTH)
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+		var result = space_state.intersect_ray(query)
+		# adding the line render to the controller
+		lineRender.points[0] = origin
+		lineRender.points[1] = end
+		
+		if result:
+			lineRender.points[1] = result.position
+			print("Collided with", + result.collider.name)
 	
 	
 
@@ -38,3 +41,12 @@ func _physics_process(_delta):
 # to the script
 func _on_lh_area_3d_area_entered(_area):
 	pass # Replace with function body.
+
+
+func _on_button_pressed(name):
+	if name == "ax_button":
+		if saberState == false:
+			saberState = true
+		else:
+			saberState = false
+		pass
