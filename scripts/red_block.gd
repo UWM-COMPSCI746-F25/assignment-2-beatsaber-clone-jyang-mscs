8ed2b5b67d8231a32f65ce6eadf2a153d33f6fe6
Jefferson Yang <jeffersonyang01@gmail.com>
2025-10-16 09:45:40 -0500

    Update to project
    Added music track. Added hit sound when ray cast hits boxes. Adjusted the boxes collision layers to their respective left or right ray cast. Tested and confirm the interaction between the controllers and their respective block to hit works. Still having issues with the line render visiblity flashing or disappearing really quickly instead of staying solid. This started when I changed the controller area collision shape from a sphere to a capsule to better represent the line render.

---
+++ scripts/red_block.gd
@@ -0,0 +1,41 @@
+extends RigidBody3D
+
+signal died
+
+var health = 1
+@onready var dir = Vector3(0, 0, 1)
+@onready var speed = 1
+
+@onready var player = get_node("root/Main/XROrigin3D")
+
+@onready var timer = %Timer
+
+@onready var hurt_sound = %HurtSound
+
+
+func _process(delta):
+	global_position += dir * (speed * delta)
+	
+	if global_position.z > 10:
+		queue_free()
+	
+func take_damage():
+	if health == 0:
+		return
+	
+	health -= 1
+	
+	if health == 0:
+		set_physics_process(false)
+		gravity_scale = 1.0
+		#var direction = global_position.direction_to(player.global_position)
+		#var randomUpwardForce = Vector3.UP * randf_range(1.0,5.0)
+		#apply_central_impulse(direction * 10 + randomUpwardForce)
+		
+		timer.start()
+		died.emit()
+		hurt_sound.play()
+
+
+func _on_timer_timeout():
+	queue_free()
--- scripts/right_controller.gd
+++ scripts/right_controller.gd
@@ -39,8 +39,9 @@ func _physics_process(_delta):
 	
 # connecting the left controller collision object
 # to the script
-func _on_rh_area_3d_area_entered(_area):
-	pass # Replace with function body.
+func _on_rh_area_3d_area_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage()
 
 func _on_button_pressed(name):
 	if name == "ax_button":
